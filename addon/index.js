1ae772b4168899f862f7887e146a7b88fcfe0983
Jonas Brusman <himynameisjonas@gmail.com>
2018-10-15 16:04:33 +0200

    No global ember

--- addon/index.js
+++ addon/index.js
@@ -1,14 +1,18 @@
 import DS from 'ember-data';
-import Ember from 'ember';
+import { get } from '@ember/object';
+import { assert } from '@ember/debug';
+import EmberArray from '@ember/array';
+import { run } from '@ember/runloop';
+import RSVP from 'rsvp';
+import { copy } from '@ember/object/internals';
 
-var get = Ember.get;
 var indexOf = Array.prototype.indexOf && function(array, item) {
   return array.indexOf(item);
-} || Ember.EnumerableUtils.indexOf;
+}
 
 var map = Array.prototype.map && function(array, cb, binding) {
   return array.map(cb, binding);
-} || Ember.EnumerableUtils.map;
+}
 
 var counter = 0;
 
@@ -91,7 +95,7 @@ export default DS.Adapter.extend({
     @return {Promise|Array}
   */
   queryFixtures(/*fixtures, query, typeClass*/) {
-    Ember.assert('Not implemented: You must override the DS.FixtureAdapter::queryFixtures method to support querying the fixture store.');
+    assert('Not implemented: You must override the DS.FixtureAdapter::queryFixtures method to support querying the fixture store.');
   },
 
   /**
@@ -147,10 +151,10 @@ export default DS.Adapter.extend({
     var fixtures = this.fixturesForType(typeClass);
     var fixture;
 
-    Ember.assert(`Unable to find fixtures for model type ${typeClass.toString()}. If you're defining your fixtures using 'Model.FIXTURES = ...'', please change it to 'Model.reopenClass({ FIXTURES: ... })'.`, fixtures);
+    assert(`Unable to find fixtures for model type ${typeClass.toString()}. If you're defining your fixtures using 'Model.FIXTURES = ...'', please change it to 'Model.reopenClass({ FIXTURES: ... })'.`, fixtures);
 
     if (fixtures) {
-      fixture = Ember.A(fixtures).findBy('id', id);
+      fixture = EmberArray(fixtures).findBy('id', id);
     }
 
     if (fixture) {
@@ -169,7 +173,7 @@ export default DS.Adapter.extend({
   findMany(store, typeClass, ids/*, snapshots*/) {
     var fixtures = this.fixturesForType(typeClass);
 
-    Ember.assert(`Unable to find fixtures for model type ${typeClass.toString()}`, fixtures);
+    assert(`Unable to find fixtures for model type ${typeClass.toString()}`, fixtures);
 
     if (fixtures) {
       fixtures = fixtures.filter(item => indexOf(ids, item.id) !== -1);
@@ -191,7 +195,7 @@ export default DS.Adapter.extend({
   findAll(store, typeClass) {
     var fixtures = this.fixturesForType(typeClass);
 
-    Ember.assert(`Unable to find fixtures for model type ${typeClass.toString()}`, fixtures);
+    assert(`Unable to find fixtures for model type ${typeClass.toString()}`, fixtures);
 
     return this.simulateRemoteCall(() => fixtures);
   },
@@ -208,7 +212,7 @@ export default DS.Adapter.extend({
   query(store, typeClass, query/*, array*/) {
     var fixtures = this.fixturesForType(typeClass);
 
-    Ember.assert(`Unable to find fixtures for model type ${typeClass.toString()}`, fixtures);
+    assert(`Unable to find fixtures for model type ${typeClass.toString()}`, fixtures);
 
     fixtures = this.queryFixtures(fixtures, query, typeClass);
 
@@ -296,7 +300,7 @@ export default DS.Adapter.extend({
     @param id
   */
   findFixtureById(fixtures, id) {
-    return Ember.A(fixtures).find((r) => '' + get(r, 'id') === '' + id);
+    return EmberArray(fixtures).find((r) => '' + get(r, 'id') === '' + id);
   },
 
   /*
@@ -308,11 +312,11 @@ export default DS.Adapter.extend({
   simulateRemoteCall(callback, context) {
     var adapter = this;
 
-    return new Ember.RSVP.Promise(function(resolve) {
-      var value = Ember.copy(callback.call(context), true);
+    return new RSVP.Promise(function(resolve) {
+      var value = copy(callback.call(context), true);
       if (get(adapter, 'simulateRemoteResponse')) {
         // Schedule with setTimeout
-        Ember.run.later(null, resolve, value, get(adapter, 'latency'));
+        run.later(null, resolve, value, get(adapter, 'latency'));
       } else {
         // Asynchronous, but at the of the runloop with zero latency
         resolve(value);
